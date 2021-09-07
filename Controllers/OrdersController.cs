b4d608273f0f71c946b07938a7a1d14bfaca05e8
Thomas Liu <s192287@student.hb.se>
2021-09-07 02:16:04 +0200

    Add order controller

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using ODataOrders.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataOrders.Controllers
+{
+    public class OrdersController : ODataController
+    {
+        private readonly Repository repo;
+
+        public OrdersController(Repository reppo)
+        {
+            this.repo = reppo;
+        }
+
+        [EnableQuery]
+        public IActionResult Get() => Ok(repo.Orders);
+
+        public IActionResult Post([FromBody] Order c)
+        {
+            repo.Orders.Add(c);
+            return Ok(c);
+        }
+    }
+}
